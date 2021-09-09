3cb750ddb4afb317071fd30a4d1a36a4058c93b0
yahya1403 <yahyamd1403@gmail.com>
2021-09-09 18:18:33 +0530

    redux

--- App.js
+++ App.js
@@ -7,38 +7,62 @@ import 'react-native-gesture-handler';
 import Todo from './Todo';
 import { data } from './data';
 import PageContextProvider from './PageContextProvider';
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 const Stack = createStackNavigator();
 //const Context = React.createContext('light');
+const INCREAMENT = "INCREAMENT";
+const DECRAEMENT = "DECRAEMENT";
 
+export const increament = () => ({ type: INCREAMENT });
+export const decreament = () => ({ type: DECRAEMENT });
+
+
+const counterReducer = (state, actions) => {
+  switch (actions.type) {
+    case "INCREAMENT":
+      return state += 1;
+    case "DECRAEMENT":
+      return state -= 1;
+    default:
+      return state = 0;
+  }
+
+}
+let store = createStore(counterReducer);
+//store.subscribe(() => console.log("s" + store.getState()))
+//store.dispatch(increament());
 function App() {
 
   return (
+
     <PageContextProvider>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-          headerMode: 'none',
-          navigationOptions: {
-            headerVisible: false,
-          }
-        }}
-          screenListeners={{
-            state: (e) => {
-              const intervalId = setInterval(() => {
-                if (data.value < 85) {
-                  data.value = 85;
-                }
-              }, 1500);
-              return () => clearInterval(intervalId);
-
-            },
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{
+            headerMode: 'none',
+            navigationOptions: {
+              headerVisible: false,
+            }
           }}
-        >
-          <Stack.Screen name="Todo" component={Todo} />
-        </Stack.Navigator>
-      </NavigationContainer>
+            screenListeners={{
+              state: (e) => {
+                const intervalId = setInterval(() => {
+                  if (data.value < 85) {
+                    data.value = 85;
+                  }
+                }, 1500);
+                return () => clearInterval(intervalId);
 
+              },
+            }}
+          >
+            <Stack.Screen name="Todo" component={Todo} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
     </PageContextProvider>
+
   );
 }
 export default App;
--- Workout.js
+++ Workout.js
@@ -1,60 +1,32 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { AreaChart, Grid, LineChart } from 'react-native-svg-charts';
 import * as shape from 'd3-shape';
-import { View, Animated, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Animated, Button, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { increament, decreament } from './App'
+
 
 export default function Workout() {
-    const fadeAnim = useRef(new Animated.Value(0)).current;
-    const data = [10, 50, 40, 95, 4, 124, 85, 251, 35, 153];
-    const heights = [new Animated.Value(10), new Animated.Value(20), new Animated.Value(30)];
-    const value = useRef(new Animated.Value(0)).current;
-    const r = (data) ? <LineChart
-        style={{ height: 100 }}
-        data={data}
-        gridMin={0}
-        curve={shape.curveNatural}
-        contentInset={{ top: 10, bottom: 10 }}
-        svg={{ stroke: 'rgb(134, 65, 244)' }}
-    /> : <Text>hi</Text>;
-    useEffect(() => {
-        // Animated.timing(fadeAnim, {
-        //     toValue: 40,
-        //     duration: 2000,
-        //     //useNativeDriver: false
-        // }).start(() => { for (let i = 0; i < 5; i++) { data.push(Math.random() + i); } console.log(data) });
-        // Animated.parallel([
-        //     Animated.timing(heights[0], { toValue: 20, duration: 2000, useNativeDriver: true }),
-        //     Animated.timing(heights[1], { toValue: 30, duration: 2000, useNativeDriver: true }),
-        //     Animated.timing(heights[2], { toValue: 40, duration: 2000, useNativeDriver: true })
-        // ]).start()
-        const intervalId = setInterval(() => {
-            if (data.length > 15) {
-                clearInterval(intervalId);
-            }
-            data.push(85);
-            // console.log(data)
-        }, 1500);
-    }, [data])
-    return (<Animated.View style={{ flex: 1, alignItem: "center", justifyContent: "center" }}>
-        {r}
+    const counter = useSelector(state => state);
+    const dispatch = useDispatch();
 
+    return (<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Button
+            onPress={() => dispatch(increament())}
+            title="Increament"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+        />
+        <Text>{counter}</Text>
+        <Button
+            onPress={() => dispatch(decreament())}
+            title="Decreament"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+        />
 
-        <TouchableOpacity style={[
-            styles.fadingContainer,
-            {
-                // Bind opacity to animated value
-                padding: fadeAnim
-            }
-        ]}>
-            <Text >HI</Text></TouchableOpacity>
-        <Text>hi</Text>
-    </Animated.View>)
+    </View>)
 
 }
-const styles = StyleSheet.create({
 
-    fadingContainer: {
-        padding: 20,
-        backgroundColor: "powderblue"
-    }
-});
+
